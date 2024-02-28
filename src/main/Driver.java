743754da27bd411be4c8221eb0370ab8ee33faae
LiamTodd <toddliam26@gmail.com>
2024-02-28 22:53:36 +1100

    Add driver.

---
+++ src/main/Driver.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("2D Simulator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
